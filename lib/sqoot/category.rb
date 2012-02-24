aafd7f776c8837aab719e15255205f991b56030c
Julius F <baldrailers@gmail.com>
2012-02-24 23:18:54 +0800

    Remove old codes from category class

--- lib/sqoot/category.rb
+++ lib/sqoot/category.rb
@@ -6,13 +6,6 @@ module Sqoot
     # @return [Hashie::Mash] category list
     def categories(options={})
       get('/v1/categories', options)
-      # options['affiliate_token'] = affiliate_token
-      # query = options
-      # response = connection.get do |req|
-      #   req.url "/v1/categories", query
-      # end
-
-      # response.body
     end
   end
 end
